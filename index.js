c3b06a536ac50b79019633b558f293e715fca48a
Tamal Anwar <tamalanwaronline@gmail.com>
2019-01-07 21:56:34 +0600

    Initial structure setup with route and pug template files

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+ * Main Server Index File
+ */
+
+var server = require('./server');
+
+// Run the server
+server.init();
+
+module.exports = server;
--- server.js
+++ server.js
@@ -1,44 +1,63 @@
 /*
- * Dependencies
+ * Server File
  */
+
+// Dependencies
 var express = require('express');
+var app = express();
 var helmet = require('helmet');
 var bodyParser = require('body-parser');
+var logger = require('logger').createLogger('./logs/development.log');
+
+// Core modules
 var fs = require('fs');
 var path = require('path');
 var http = require('http');
 var https = require('https');
-var logger = require('logger').createLogger('./logs/development.log');
-var app = express();
 
-// Includes
-var config = require('./config.js');
+// Helper files
+var config = require('./helpers/config');
+var routes = require('./routes/main');
 
-// Helmet for security
-app.use(helmet());
+// Server
+var server = {};
 
-// View engine
-app.set('view engine', 'pug');
+server.init = function() {
+  // Helmet for security
+  app.use(helmet());
 
-// Body parser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+  // View engine
+  app.set('view engine', 'pug');
+  app.use(express.static(path.join(__dirname, 'public')));
 
-// HTTP Server
-http.createServer(app)
-  .listen(config.httpPort, () => {
-    
-    if(config.name === 'development') {
-      let message = `We have a ${config.name} server running on PORT: ${config.httpPort}`;
+  // Body parser
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  // Routes
+  app.use('/', routes);
+
+  // HTTP Server
+  http.createServer(app).listen(config.httpPort, () => {
+    if (config.name === 'development') {
+      let message = `We have a ${config.name} server running on PORT: ${
+        config.httpPort
+      }`;
       logger.info(message);
       console.log(message);
     }
-
   });
 
-// HTTPS Server
-https.createServer({
-  cert: fs.readFileSync('./ssl/server.cert'),
-  key: fs.readFileSync('./ssl/server.key')
-}, app)
-  .listen(config.httpsPort);
+  // HTTPS Server
+  https
+    .createServer(
+      {
+        cert: fs.readFileSync('./ssl/server.cert'),
+        key: fs.readFileSync('./ssl/server.key')
+      },
+      app
+    )
+    .listen(config.httpsPort);
+};
+
+module.exports = server;
